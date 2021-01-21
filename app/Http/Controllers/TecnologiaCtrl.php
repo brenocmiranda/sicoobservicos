41a498c3b2af4d29a5f60d1ec85e7059fffb10df
Breno de Carvalho Miranda <Brenoc4133_00@sicoob.coop>
2021-01-21 10:49:17 -0300

    Atualizar

--- app/Http/Controllers/TecnologiaCtrl.php
+++ app/Http/Controllers/TecnologiaCtrl.php
@@ -541,7 +541,7 @@ class TecnologiaCtrl extends Controller
         }else{
         	return datatables()->of(AtivosEquipamentos::all())
 	            ->editColumn('nome1', function(AtivosEquipamentos $dados){ 
-	                return '<a href="javascript:void(0)" id="detalhes">'.$dados->nome'</a>';
+	                return '<a href="javascript:void(0)" id="detalhes">'.$dados->nome.'</a>';
 	            })
 	            ->editColumn('status1', function(AtivosEquipamentos $dados){
 	                return '<label class="badge'.($dados->status == 1 ? " badge-success" : " badge-danger").'">'.($dados->status == 1 ? "Ativo" : "Desativado").'</label>';
