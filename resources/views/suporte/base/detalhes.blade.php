628a8873cee2d0c675cffe9a72fac269370209a5
Sicoob Serviços <sicoobservicos@sicoob.coop>
2021-03-16 11:48:41 -0300

    Atualizar

--- resources/views/suporte/base/detalhes.blade.php
+++ resources/views/suporte/base/detalhes.blade.php
@@ -61,9 +61,10 @@
           </div>
           <div class="col-12 text-justify">
             <div>
-              <h4>{{$dados->titulo}}</h4>
+              <h4>{{$dados->titulo}} <small class="text-capitalize">{{$dados->tipo}}</small></h4>
               <h5>{{$dados->RelationAmbientes->nome}} &#183 {{$dados->RelationFontes->nome}}</h5>
               <label>{{$dados->subtitulo}}</label> 
+
               @foreach($dados->RelationArquivos as $arquivos)
               <div class="row mx-auto"> 
                 <a href="{{asset('storage/app/'.$arquivos->endereco)}}" target="_blank" class="row col-12">
